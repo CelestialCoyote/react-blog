2374e430b35986fba4debb8b49a5b30770c50928
Paul Stearns <celestialcoyote@gmail.com>
2023-03-20 15:11:24 -0700

    added user authentication to Articles page

--- frontend/src/pages/Article/Article.js
+++ frontend/src/pages/Article/Article.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import axios from 'axios';
+import useUser from '../../customHooks/useUser';
 import CommentsList from '../../components/CommentsList/CommentsList';
 import NotFound from "../NotFound";
 import AddCommentForm from '../../components/AddCommentForm/AddCommentForm';
@@ -11,6 +12,7 @@ import './Article.css';
 const Article = () => {
 	const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 	const { articleId } = useParams();
+	const { user, isLoading } = useUser();
 
 	useEffect(() => {
 		const loadArticleInfo = async () => {
@@ -36,7 +38,10 @@ const Article = () => {
 		<div className='article'>
 			<h1>{article.title}</h1>
 			<div className='upvotes-section'>
-				<button onClick={addUpvote}>Upvote</button>
+				{user
+					? <button onClick={addUpvote}>Upvote</button>
+					: <button>Log in to Upvote</button>
+				}
 				<p>This article has {articleInfo.upvotes} upvote(s).</p>
 			</div>
 
@@ -44,10 +49,14 @@ const Article = () => {
 				<p key={i}>{paragraph}</p>
 			))}
 
-			<AddCommentForm
-				articleName={articleId}
-				onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}
-			/>
+			{user
+				? <AddCommentForm
+					articleName={articleId}
+					onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}
+				/>
+				: <button>Log in to add a comment</button>
+			}
+
 			<CommentsList comments={articleInfo.comments} />
 		</div>
 	);
