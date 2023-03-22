c644816db9dc0b3cca72d79ab83b42b78564d721
Paul Stearns <celestialcoyote@gmail.com>
2023-03-22 16:06:01 -0700

    Creating production files.

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,7 +4,8 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    //const client = new MongoClient('mongodb://127.0.0.1:27017');
+	const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.tqecnif.mongodb.net/?retryWrites=true&w=majority`);
 	await client.connect();
 
 	db = client.db('react-blog-db');
